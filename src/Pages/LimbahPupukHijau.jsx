ad8caad5a438d8bc033d5352103c742dfe007f28
dortea ribka tadete <dortearibkatadete17.com>
2024-12-19 11:52:33 +0800

    resolve conflicts

--- src/Pages/LimbahPupukHijau.jsx
+++ src/Pages/LimbahPupukHijau.jsx
@@ -3,7 +3,7 @@ import NavbarUser from '../Components/NavbarUser'
 import DefinisiPupukHijau from '../Sections/Limbah/PupukHijau/DefinisiPupukHijau'
 import AlatPupukHijau from '../Sections/Limbah/PupukHijau/AlatPupukHijau'
 import LangkahPupukHijau from '../Sections/Limbah/PupukHijau/LangkahPupukHijau'
-import VideoPupukHijau from '../Sections/Limbah/PupukHijau/VideoPupukHIjau'
+import VideoPupukHijau from '../Sections/Limbah/PupukHijau/VideoPupukHijau'
 import Footer from '../Components/Footer'
 
 export default function LimbahPupukHijau() {
